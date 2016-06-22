1170624a878c029c90ae6fdeb2fcaeb13c652de6
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-06-22 03:19:27 +0100

    Simple get request send response example

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+});
+
+app.get('/Books', function (req, res) {
+    res.send('Hello Books');
+});
+
 app.listen(port, function (err) {
     console.log('Running server on port ' + port);
 
